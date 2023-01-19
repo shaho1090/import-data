ec415c72340a8372d8d0bad22753339ddace3f05
shaho1090 <shaho.parvini@gmail.com>
2023-01-19 17:56:46 +0330

    add index to some fields in logs table

---
+++ database/migrations/2023_01_19_122046_make_date_index_in_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->index('service_name');
+            $table->index('status_code');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('logs', function (Blueprint $table) {
+            $table->dropIndex('service_name');
+            $table->dropIndex('status_code');
+
+        });
+    }
+};
