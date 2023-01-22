426ec7c2f469651981414461a763bdd36e1ded6d
shaho1090 <shaho.parvini@gmail.com>
2023-01-22 16:28:07 +0330

    fixing date format

--- database/factories/TextLogFileLineRandomCreator.php
+++ database/factories/TextLogFileLineRandomCreator.php
@@ -31,19 +31,19 @@ class TextLogFileLineRandomCreator
             ]
         ]);
 
-        $date = $this->date->addSeconds(5)->format("d/M/Y:H:i:s");
+        $date = $this->date->addSeconds(5);
         $service = $services->random();
 
         $this->arrayLine = [
             "service_name" => $service['service-name'],
-            "date" => $date,
+            "date" => $date->toDateTimeString(),
             "http_verb" => "POST",
             "path" =>  $service['path'],
             "http_protocol" => "HTTP/1.1",
             "status_code" => $statusCode,
         ];
 
-        $this->line = $service['service-name'] . ' ' . "-" . ' [' . $date . '] ' . '"' . 'POST ' . $service['path']
+        $this->line = $service['service-name'] . ' ' . "-" . ' [' . $date->format("d/M/Y:H:i:s") . '] ' . '"' . 'POST ' . $service['path']
             . ' HTTP/1.1' . '" ' . $statusCode;
 
         return $this;
