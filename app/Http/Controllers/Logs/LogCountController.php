c42976e4016d626498c8036c54fc367afa87ba7c
shaho1090 <shaho.parvini@gmail.com>
2023-01-23 12:01:35 +0330

    using log service

--- app/Http/Controllers/Logs/LogCountController.php
+++ app/Http/Controllers/Logs/LogCountController.php
@@ -5,16 +5,20 @@ namespace App\Http\Controllers\Logs;
 use App\Filters\LogQueryFilter;
 use App\Http\Controllers\Controller;
 use App\Models\Log;
+use App\Services\Interfaces\LogServiceInterface;
 use Illuminate\Http\JsonResponse;
 
 class LogCountController extends Controller
 {
+    public function __construct(
+        private LogServiceInterface $logService)
+    {
+    }
+
     public function index(LogQueryFilter $filters): JsonResponse
     {
-        $count = Log::query()->filter($filters)->count();
-
         return response()->json([
-            'count' => $count
+            'count' => $this->logService->filterAndCount($filters)
         ]);
     }
 }
